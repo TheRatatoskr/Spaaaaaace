4bfa3f8f907be05809014019c71f5ecd1443d1aa
TheRatatoskr <joseph.ewert@gmail.com>
2024-01-07 10:48:06 -0600

    smoothed out vertical movement

--- Assets/Scripts/Player/MovementTargetController.cs
+++ Assets/Scripts/Player/MovementTargetController.cs
@@ -6,7 +6,10 @@ public class MovementTargetController : MonoBehaviour
 {
     [SerializeField] private GameObject _target;
 
+
     [SerializeField] private InputReader _input;
+
+    [SerializeField] private float _moveSpeed;
     
     private Vector2 _joystickSpot;
 
@@ -17,7 +20,8 @@ public class MovementTargetController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        _target.transform.localPosition = new Vector3(_target.transform.localPosition.x, _joystickSpot.y);
+        _target.transform.Translate(new Vector3(0, _joystickSpot.y)*Time.deltaTime * _moveSpeed, transform);
+        //_target.transform.localPosition = new Vector3(_target.transform.localPosition.x, _joystickSpot.y);
     }
 
     private void UpdateJoystickLocation(Vector2 joystickLocation)
